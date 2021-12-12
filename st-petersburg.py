dfe764064becf7749f363c6e8860c23d337f69cb
Ayberk <ayberksadic@gmail.com>
2021-12-12 14:56:46 +0300

    initial

--- st-petersburg.py
+++ st-petersburg.py
@@ -7,8 +7,9 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.lib.polynomial import polyfit
 
-noToss=8
+noToss=5
 pot=np.zeros(noToss)
 wealth=10*np.ones(noToss)
 for k in range(0,noToss):
@@ -18,16 +19,28 @@ for k in range(0,noToss):
 utilityBefore=np.log(wealth)    
 utilityAfter=np.log(wealth+pot)
 price=utilityAfter-utilityBefore
+np.polyfit(wealth,utilityBefore,1)
 
 print(pot)
 print(wealth)
 print(utilityBefore)
 print(utilityAfter)
 
-plt.title("St.Petersburg Rewards")
+""" plt.title("St.Petersburg Rewards")
 plt.xlabel("Number of Tosses")
 plt.ylabel("Pot")
 plt.plot(range(noToss),pot,color="red")
+plt.grid()
+plt.show() """
+
+w=np.arange(1000)
+u=np.log(w)
+
+plt.title("Bernoulli's Utility Function")
+plt.xlabel("Wealth [w]")
+plt.ylabel("Utility [u]")
+plt.plot(w,u,color="red")
+""" plt.xscale('log') """
 plt.grid()
 plt.show()
 
